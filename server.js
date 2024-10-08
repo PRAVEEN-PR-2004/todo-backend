c4525f6539f6f4ac2551fd391922f43abe3dddd0
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2024-10-08 18:16:37 +0530

    cros

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const cors = require("cors");
+const cors = require("cros");
 
 app.use(express.json());
 app.use(cors());
