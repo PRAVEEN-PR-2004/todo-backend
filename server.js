19b8d98141b547fb8f426b9a686ec10f2e730160
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2024-10-08 17:40:27 +0530

    url

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const app = express();
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "https://chat-todo.netlify.app/", // Replace with your frontend URL
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
 // Connect to MongoDB Atlas
 mongoose
